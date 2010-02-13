c20f8e05c09fd71a6a11b25f8d3ad3c2e4d38768
Joseph Turian <turian@gmail.com>
2010-02-13 15:49:36 -0500

    Skip model building if model file exists.

--- scripts/train-and-evaluate.py
+++ scripts/train-and-evaluate.py
@@ -69,12 +69,16 @@ if os.path.exists(scoredevalfile):
     print >> sys.stderr, "%s exists. STOPPING" % scoredevalfile
     sys.exit(0)
 
-cmd = "cat %s | %s %s > %s" % (join(datadir, trainfile), featurescript, options.features, featurestrainfile)
-run(cmd)
+if os.path.exists(modelfile + ".gz"):
+    run("gunzip %s.gz" % modelfile)
 
-cmd = "crfsuite learn -p algorithm=sgd -p feature.possible_transitions=1 -p feature.possible_states=1  -p regularization.sigma=%s -m %s %s 2>&1 | tee %s.err" % (options.l2, modelfile, featurestrainfile, modelfile)
-run(cmd)
-run("gzip -f %s" % featurestrainfile)
+if not os.path.exists(modelfile):
+    cmd = "cat %s | %s %s > %s" % (join(datadir, trainfile), featurescript, options.features, featurestrainfile)
+    run(cmd)
+
+    cmd = "crfsuite learn -p algorithm=sgd -p feature.possible_transitions=1 -p feature.possible_states=1  -p regularization.sigma=%s -m %s %s 2>&1 | tee %s.err" % (options.l2, modelfile, featurestrainfile, modelfile)
+    run(cmd)
+    run("gzip -f %s" % featurestrainfile)
 
 cmd = "cat %s | %s %s > %s" % (join(datadir, evalfile), featurescript, options.features, featuresevalfile)
 run(cmd)
